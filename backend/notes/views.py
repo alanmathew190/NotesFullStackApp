2a7bf2447d85480b50d0face96c86370ebf8aaf2
Alan <alanmathew190@gmail.com>
2025-11-21 11:20:19 +0530

    push backend final

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import NoteInfo
+from .serializers import NoteSerializer
 # Create your views here.
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset=NoteInfo.objects.all()
+    serializer_class=NoteSerializer
+
